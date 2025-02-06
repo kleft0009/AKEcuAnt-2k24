c9b286d17706a925f7012f4b3e46475fb1ac8af8
kleft0009 <azanakevin16@gmail.com>
2025-02-06 18:56:38 -0500

    Actualizacion del Script

--- AKScript/AKHormiguero.sql
+++ AKScript/AKHormiguero.sql
@@ -1,4 +1,4 @@
--- database: ../database/AKHormigueroVirtual.sqlite
+-- database: ../AKdatabase/AKHormigueroVirtual.sqlite
 /*
 CopyRight: 2k25 EPN
 autor: kleft
@@ -25,19 +25,22 @@ CREATE TABLE
         IdCatalogoTipo INTEGER NOT NULL REFERENCES Catalogo (IdCatalogo),
         IdCatalogoSexo INTEGER NOT NULL REFERENCES Catalogo (IdCatalogo),
         IdCatalogoEstado INTEGER NOT NULL REFERENCES Catalogo (IdCatalogo),
+        IdCatalogoIngestaNativa INTEGER NOT NULL REFERENCES Catalogo (IdCatalogo),
+        IdCatalogoGenoAllimento INTEGER NOT NULL REFERENCES Catalogo (IdCatalogo),
         Nombre VARCHAR(20) NOT NULL,
         Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
         FechaCreacion DATETIME NOT NULL DEFAULT (DATETIME('now', 'localtime')),
         FechaModificacion DATETIME
     );
 
+DROP VIEW IF EXISTS vwHormiga;
+
 DROP VIEW IF EXISTS vwHormigaTipo;
 
 CREATE VIEW
-    vwHormigaTipo AS
+    vwHormiga AS
 SELECT
     h.IdCatalogo,
-    h.IdCatalogoPadre,
     h.Nombre,
     h.Detalle,
     h.Estado,
@@ -47,43 +50,13 @@ FROM
     Catalogo p
     JOIN Catalogo h ON h.IdCatalogoPadre = p.IdCatalogo
 WHERE
-    p.Nombre = 'HormigaTipo';
-
-DROP VIEW IF EXISTS vwHormigaSexo;
-
-CREATE VIEW
-    vwHormigaSexo AS
-SELECT
-    h.IdCatalogo,
-    h.IdCatalogoPadre,
-    h.Nombre,
-    h.Detalle,
-    h.Estado,
-    h.FechaCreacion,
-    h.FechaModificacion
-FROM
-    Catalogo p
-    JOIN Catalogo h ON h.IdCatalogoPadre = p.IdCatalogo
-WHERE
-    p.Nombre = 'HormigaSexo';
-
-DROP VIEW IF EXISTS vwHormigaEstado;
-
-CREATE VIEW
-    vwHormigaEstado AS
-SELECT
-    h.IdCatalogo,
-    h.IdCatalogoPadre,
-    h.Nombre,
-    h.Detalle,
-    h.Estado,
-    h.FechaCreacion,
-    h.FechaModificacion
-FROM
-    Catalogo p
-    JOIN Catalogo h ON h.IdCatalogoPadre = p.IdCatalogo
-WHERE
-    p.Nombre = 'HormigaEstado';
+    p.Nombre IN (
+        'HormigaTipo',
+        'HormigaSexo',
+        'HormigaEstado',
+        'HormigaGenoAlimento',
+        'HormigaIngestaNativa'
+    );
 
 INSERT INTO
     Catalogo (IdCatalogoPadre, Nombre, Detalle)
@@ -94,25 +67,39 @@ VALUES
         'HormigaSexo',
         'tipo de sexo de las hormigas'
     ),
-    (NULL, 'HormigaEstado', 'estado de las hormigas');
+    (NULL, 'HormigaEstado', 'estado de las hormigas'),
+    (
+        NULL,
+        'HormigaIngestaNativa',
+        'ingesta nativa de las hormigas'
+    ),
+    (
+        NULL,
+        'HormigaGenoAlimento',
+        'genoAlimento de las hormigas'
+    );
 
 INSERT INTO
     Catalogo (IdCatalogoPadre, Nombre, Detalle)
 VALUES
     (1, 'Larva', 'tipos de hormigas'),
     (1, 'Soldado', 'tipos de hormigas'),
-    (1, 'Zangano', 'tipos de hormigas'),
     (2, 'Macho', 'sexo de la hormiga'),
     (2, 'Hembra', 'sexo de la hormiga'),
+    (2, 'Asexual', 'sexo de la hormiga'),
     (3, 'Viva', 'estado de la hormiga'),
-    (3, 'Muerta', 'estado de la hormiga');
+    (3, 'Muerta', 'estado de la hormiga'),
+    (4, 'Carnivoro', 'ingesta nativa'),
+    (5, 'XY', 'genoAlimento');
 
 INSERT INTO
     Hormiga (
         IdCatalogoTipo,
         IdCatalogoSexo,
         IdCatalogoEstado,
+        IdCatalogoIngestaNativa,
+        IdCatalogoGenoAllimento,
         Nombre
     )
 VALUES
-    (4, 7, 9, 'kleft');
+    (7, 8, 11, 13, 14, 'kleft');
